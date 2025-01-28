70f65a0104629c5d7ebcb7d11626e059188b0c4b
Victor Prado <victor.prasa@gmail.com>
2025-01-27 22:08:09 -0300

    Adicionado legalidades

--- src/api/api.js
+++ src/api/api.js
@@ -1,51 +1,67 @@
 import axios from "axios";
 
 export async function fetchRss() {
-    try {
-        const url = 'http://localhost:8002/rss';
-        const res = await axios.get(url);
-        // console.log('res', res)
-        return res.data.items
-    } catch (error) {
-        console.log('Error  fetching or parsing RSS feed: ', error);
-    }
-    return []
+  try {
+    const url = 'http://localhost:8002/rss';
+    const res = await axios.get(url);
+    // console.log('res', res)
+    return res.data.items
+  } catch (error) {
+    console.log('Error  fetching or parsing RSS feed: ', error);
+  }
+  return []
 }
 
 export async function fetchCollection() {
-    try {
-        const url = 'http://localhost:8002/cards';
-        const res = await axios.get(url);
-        // console.log('cards', res.data)
-        return res.data
-    } catch (error) {
-        console.log('Error  fetching or parsing cards: ', error);
-    }
-    return []
+  try {
+    const url = 'http://localhost:8002/cards';
+    const res = await axios.get(url);
+    // console.log('cards', res.data)
+    return res.data
+  } catch (error) {
+    console.log('Error  fetching or parsing cards: ', error);
+  }
+  return []
 }
 
 export async function fetchMostValuable(total) {
-    try {
-        const cards = await fetchCollection()
-        const mostValuable = cards.toSorted((a, b) => {
-            return b.prices.usd - a.prices.usd
-        }).slice(0, total)
-        // console.log('expensives: ', mostValuable.map(c => c.prices.usd))
-        return mostValuable
-    } catch (error) {
-        console.log('Error  fetching or parsing valuable cards: ', error);
-    }
-    return []
+  try {
+    const cards = await fetchCollection()
+    const mostValuable = cards.toSorted((a, b) => {
+      return b.prices.usd - a.prices.usd
+    }).slice(0, total)
+    // console.log('expensives: ', mostValuable.map(c => c.prices.usd))
+    return mostValuable
+  } catch (error) {
+    console.log('Error  fetching or parsing valuable cards: ', error);
+  }
+  return []
+}
+
+function toTitleFormat(text) {
+  const words = text.split(' ').map((w) => {
+    return w.charAt(0).toUpperCase() + w.slice(1)
+  })
+  return words.join(' ')
 }
 
 export async function fetchCard(cardId) {
-    try {
-        const cards = await fetchCollection()
-        const card = cards.find(c => c.id === cardId)
-        console.log('card ', card)
-        return card
-    } catch (error) {
-        console.log('Error  fetching or parsing card: ', error);
-    }
-    return null
+  try {
+    const cards = await fetchCollection()
+    const card = cards.find(c => c.id === cardId)
+    card.legalities = Object.entries(card.legalities).map((item, index) => {
+      //const legality = item.replace('_', ' ')
+      const value = toTitleFormat(item[1].replace('_', ' '))
+      const prop = toTitleFormat(item[0])
+      console.log(prop + ': ' + value)
+      const obj = {}
+      obj[prop] = value
+      return [prop, value]
+    })
+    console.log('card.legalities ', card.legalities)
+    return card
+  } catch (error) {
+    console.log('Error  fetching or parsing card: ', error);
+  }
+  return null
 }
--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -5,8 +5,9 @@
     </div>
     <div v-else>
       <div class="fit row wrap justify-center items-center content-start">
+
         <div>
-          <q-img class="q-mx-aut" :src="card.image_uris.normal" style=" min-width: 240px;" />
+          <q-img :src="card.image_uris.normal" style=" min-width: 230px; " />
         </div>
         <div class="text-body1 q-pa-md" style="max-width: 340px;">
           <h4>{{ card.name }}</h4>
@@ -14,6 +15,52 @@
             {{ card.rarity }} - {{ card.set_name }}
           </div>
           {{ card.oracle_text }}
+          <!-- <q-separator color="neutral" /> -->
+        </div>
+      </div>
+      <div class="text-body2 q-pt-sm row ">
+        <div v-for="(item, index) in card.legalities.slice(0, 4)" :key="index"></div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(0, 4)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
+        </div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(4, 8)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
+        </div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(8, 12)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
+        </div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(12, 16)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
+        </div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(16, 20)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
+        </div>
+        <div>
+          <div class="q-px-sm row" v-for="(item, index) in card.legalities.slice(20, 24)" :key="index">
+            <div>{{ item[0] }}</div>
+            <div class="q-pr-sm">:</div>
+            <div>{{ item[1] }}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -32,17 +79,24 @@ export default defineComponent({
     const card = ref(null)
     const route = useRoute()
     const loading = ref(true);
+    const legalitiesColSize = ref(4)
+    const legalitiesCols = ref([])
 
     onMounted(
       async () => {
         const cardId = route.params.id
         card.value = await fetchCard(cardId)
         loading.value = false
+        legalitiesCols.value = (start, stop, step) =>
+          Array.from(
+            { length: (stop - start) / step + 1 },
+            (value, index) => start + index * step
+          );
       }
     )
 
     return {
-      card, loading
+      card, loading, legalitiesColSize
     }
   }
 });
